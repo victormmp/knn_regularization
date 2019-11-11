437ba261c44a35c86d03b9aefd0835e851952d59
victormmp <victormmp@outlook.com>
2019-11-11 13:40:31 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example of use of KNN network to change dataset labels, to reduce training error and weight norm.',
+    author='Victor Marcius Magalhaes Pinto',
+    license='MIT',
+)
